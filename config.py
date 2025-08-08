7781732b0338e81cb2f00077cf685db7efcc976b
poper12 <samyakdhengle89@gmail.com>
2025-08-08 22:26:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config(object):
     # database config
     DB_NAME = os.environ.get("DB_NAME","Cluster0")     
     DB_URL  = os.environ.get("DB_URL","mongodb+srv://techaktg:QWJeXZsWDpeZ7WR@cluster0.syvlm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    PORT = os.environ.get("PORT", "8080")
+    PORT = os.environ.get("PORT", "8060")
  
     # other configs
     BOT_UPTIME  = time.time()
